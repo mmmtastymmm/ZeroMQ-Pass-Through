d8a2bd22690ca9174c696a373d2780e470c99c69
Thomas Marks <tmsluigi@gmail.com>
2022-07-31 13:27:34 -0600

    Now parse all the input args for publisher

--- src/GenerateMessages.cpp
+++ src/GenerateMessages.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <boost/program_options.hpp>
 #include <thread>
+#include <utility>
 #include <zmq.hpp>
 #include "GenerateMessages.h"
 
@@ -11,29 +12,17 @@ boost::program_options::options_description get_description()
     description.add_options()
         // clang-format off
             ("help,h", "produce help message")
-            ("address", po::value<std::string>()->default_value("0.0.0.0"), "Address to publish to")
-            ("port", po::value<std::string>()->default_value("9090"), "Port to publish to")
-            ("message-count", po::value<long long>()->default_value(500), "How many messages to send before exit")
-            ("message-text", po::value<std::string>()->default_value("Hello world"), "What text to send on the message")
-            ("append-counter", po::value<bool>()->default_value(true), "If a ascending unique id should be appended to the message text")
+            ("address", po::value<decltype(GenerateMessages::InputArgs::address)>()->default_value("0.0.0.0"), "Address to publish to")
+            ("port", po::value<decltype(GenerateMessages::InputArgs::port)>()->default_value("9090"), "Port to publish to")
+            ("message-count", po::value<decltype(GenerateMessages::InputArgs::message_count)>()->default_value(500), "How many messages to send before exit")
+            ("message-text", po::value<decltype(GenerateMessages::InputArgs::message_text)>()->default_value("Hello world"), "What text to send on the message")
+            ("append-counter", po::value<decltype(GenerateMessages::InputArgs::append_counter)>()->default_value(true), "If a ascending unique id should be appended to the message text")
             ;
     // clang-format on
     return description;
 }
 
-struct InputArgs
-{
-    std::string ip;
-    std::string port;
-    long long message_count;
-
-    InputArgs(std::string ip, std::string port, long long message_count)
-        : ip(std::move(ip)), port(std::move(port)), message_count(message_count)
-    {
-    }
-};
-
-InputArgs parse_input_args(int argc, char** argv)
+GenerateMessages::InputArgs parse_input_args(int argc, char** argv)
 {
     namespace po = boost::program_options;
     po::variables_map variables_map;
@@ -43,9 +32,16 @@ InputArgs parse_input_args(int argc, char** argv)
         std::cout << description << std::endl;
         std::exit(1);
     }
-    return {variables_map.at("ip").as<std::string>(),
-            variables_map.at("port").as<std::string>(),
-            variables_map.at("message-count").as<long long>()};
+    return {
+        variables_map.at("address").as<decltype(GenerateMessages::InputArgs::address)>(),
+        variables_map.at("port").as<decltype(GenerateMessages::InputArgs::port)>(),
+        variables_map.at("message-count")
+            .as<decltype(GenerateMessages::InputArgs::message_count)>(),
+        variables_map.at("message-text")
+            .as<decltype(GenerateMessages::InputArgs::message_text)>(),
+        variables_map.at("append-counter")
+            .as<decltype(GenerateMessages::InputArgs::append_counter)>(),
+    };
 }
 
 int GenerateMessages::main(int argc, char** argv)
@@ -54,7 +50,7 @@ int GenerateMessages::main(int argc, char** argv)
     zmq::context_t zmq_context;
     zmq::socket_t socket(zmq_context, zmq::socket_type::pub);
 
-    auto address = "tcp://" + input_args.ip + ":" + input_args.port;
+    auto address = "tcp://" + input_args.address + ":" + input_args.port;
     std::cout << "Now listening on: " << address << std::endl;
     socket.bind(address);
     std::cout << "Will send this many messages: " << input_args.message_count
@@ -69,4 +65,16 @@ int GenerateMessages::main(int argc, char** argv)
 
     std::cout << "Done!" << std::endl;
     return 0;
+}
+GenerateMessages::InputArgs::InputArgs(std::string address,
+                                       std::string port,
+                                       long long int messageCount,
+                                       std::string messageText,
+                                       bool appendCounter)
+    : address(std::move(address)),
+      port(std::move(port)),
+      message_count(messageCount),
+      message_text(std::move(messageText)),
+      append_counter(appendCounter)
+{
 }
--- src/GenerateMessages.h
+++ src/GenerateMessages.h
@@ -4,10 +4,25 @@
 
 #ifndef ZMQ_PASS_THROUGH_GENERATEMESSAGES_H
 #define ZMQ_PASS_THROUGH_GENERATEMESSAGES_H
+#include <string>
 
 class GenerateMessages
 {
 public:
+    struct InputArgs
+    {
+        std::string address;
+        std::string port;
+        long long message_count;
+        std::string message_text;
+        bool append_counter{};
+
+        InputArgs(std::string address,
+                  std::string port,
+                  long long int messageCount,
+                  std::string messageText,
+                  bool appendCounter);
+    };
     static int main(int argc, char** argv);
 };
 
